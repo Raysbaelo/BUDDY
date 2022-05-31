6544121ae24d44d341eb152f319fc3762725e140
lisa scanzi <lisascanzi@gmail.com>
2022-05-31 18:04:05 +0200

    debut

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tasks#index'
-  resources :buddy, only: [:show]
-  resources :task do
-    resources :occurance
+  resources :buddies, only: [:show]
+  resources :users, only: [:update, :index]
+  resources :tasks do
+    resources :occurances
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
