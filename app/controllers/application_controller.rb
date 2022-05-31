acdddb1d398c33266f7466bd747180dc8d8b7f9e
zsh <rayane-09-38@live.fr>
2022-05-31 18:32:11 +0200

    no show navbar on landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def disable_nav
+    @disable_nav = true
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'tasks#index'
-  resources :buddy, only: [:show]
-  resources :task do
-    resources :occurance
-  end
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
