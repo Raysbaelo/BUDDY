d507231bf5c28f2ba2a59d23d2a6e254ee1712bb
lisa scanzi <lisascanzi@gmail.com>
2022-06-01 12:47:58 +0200

    questionnaire

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#loading'
+  # match "user_root" => 'superpowers#landing'
+
   resources :buddies, only: [:show]
   resources :users, only: [:update, :index]
   resources :tasks do
