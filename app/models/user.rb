9259edaa4be372bc2621ee22bafa3305f85be977
lisa scanzi <lisascanzi@gmail.com>
2022-06-02 15:07:58 +0200

    merci benji

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tasks, dependent: :destroy
 
-
-  # validates :category_health, numericality: {less_than_or_equal_to: 2}
-  # validates :category_sport, numericality: { less_than_or_equal_to: 2}
-  # validates :category_business, numericality: { less_than_or_equal_to: 2 }
-  # validates :category_hobby, numericality: { less_than_or_equal_to: 2}
 end
